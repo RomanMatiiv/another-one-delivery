0d89c458225c072d233057c53c6c807fbd640309
matiiv <matiiv.r.a@gmail.com>
2024-01-26 09:12:11 +0300

    add todo

--- user-manager/app/main.py
+++ user-manager/app/main.py
@@ -12,3 +12,6 @@ app.include_router(router_v1.router)
 # todo настроить запуск тестов с отчетом о покрытии
 # todo сделать ручку metrics(prometeus)
 # todo сделать ручку ready
+# todo настроить запуск статических анализаторов mypy и pyright
+# todo настроить запуск тестов и линтера в ci
+# todo разобраться с ошибкой вывода make "make: *** [test] Error"
